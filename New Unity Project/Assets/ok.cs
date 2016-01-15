75135b5ff8b63250491d97342c514f87f98b8c3f
Wesley Starcke <wstarcke@hotmail.com>
2016-01-14 18:20:02 -0700

    I am committing to these changes.

---
+++ New Unity Project/Assets/ok.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ok : MonoBehaviour {
+	int myInt = 6;
+
+	int multiplybytwo (int number){
+		int ret = number * 2;
+		return ret;
+	}
+
+	// Use this for initialization
+	void Start () {
+		myInt = multiplybytwo (myInt);
+		Debug.Log (myInt);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
