5c70fdebe3e6ae287bc5b26562a9ea4e0a4882b8
Wesley Starcke <wstarcke@hotmail.com>
2016-01-21 17:55:37 -0700

    Conventions and syntax

--- New Unity Project/Assets/Conventions and syntax.cs
+++ New Unity Project/Assets/Conventions and syntax.cs
@@ -5,11 +5,11 @@ public class Conventionsandsyntax : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
