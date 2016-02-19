2999b91b39c648f3e27da76dac2e43d750818b4b
Wesley Starcke <wstarcke@hotmail.com>
2016-02-18 17:43:48 -0700

    loops

---
+++ New Unity Project/Assets/If statements.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine; using System.Collections;
+public class BoolScript : MonoBehaviour 
+{
+	bool isFlying = false;
+	//A bool can be one of two values. 'true' or 'false' bool isFlying = false;
+
+	// Use this for initialization 
+	void Start () { 
+
+	//Use '==' to test whether or not isFlying is false
+
+	if(isFlying == false) 
+	{
+
+		Debug.Log("isFlying is now " + isFlying); 
+
+		//get the return value from FlyingTest and set it to isFlying isFlying = FlyingTest(); Debug.Log("isFlying is now " + isFlying);
+
+	} 
+	else if(isFlying == true) 
+	{
+
+		Debug.Log("isFlying is already true");
+
+	}
+	}
+
+}
---
+++ New Unity Project/Assets/WhileLoops.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhileLoops : MonoBehaviour 
+{
+	int cupsinthesink = 4;
+
+	// Use this for initialization
+	void Start () 
+	{
+		while (cupsinthesink > 0) 
+		{
+			Debug.Log ("I have shattered a cup on the floor.");
+
+			cupsinthesink --;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
