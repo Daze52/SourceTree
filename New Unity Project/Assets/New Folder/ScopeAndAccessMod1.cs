9d70eced126885e4185088d49fff7bd2355f2cca
Wesley Starcke <wstarcke@hotmail.com>
2016-03-08 18:01:15 -0700

    Commitment

---
+++ New Unity Project/Assets/New Folder/ScopeAndAccessMod1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeAndAccessMod1 : MonoBehaviour 
+{
+	public int Red = 1;
+
+	public int Blue = 2;
+
+	int Purple = 0;
+
+	int mix (int color1, int color2)
+	{
+		int ret = color1 + color2;
+		
+		return ret;
+	}
+	void Start () 
+	{
+		Purple = mix (Red, Blue);
+
+		print ("Purple is " + Purple);
+	}
+}
