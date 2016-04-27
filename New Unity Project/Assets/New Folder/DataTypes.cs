339363bfc12600ee4f5499492662a97867824722
Wesley Starcke <wstarcke@hotmail.com>
2016-04-26 19:17:32 -0600

    Classes/Data Types

---
+++ New Unity Project/Assets/New Folder/DataTypes.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataTypes : MonoBehaviour 
+{
+	public class Colors
+	{
+		public int red;
+		public int blue;
+		public int green;
+		
+		public Colors(int r, int b, int g)
+		{
+		
+			red = r;
+			blue = b;
+			green = g;
+		}
+	}
+	
+	public Colors myColor = new Colors(1, 2, 3);
+}
