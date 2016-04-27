f54c050be71a3896492ee996332337d4b6ac2501
Wesley Starcke <wstarcke@hotmail.com>
2016-04-26 18:21:32 -0600

    Enum

---
+++ New Unity Project/Assets/New Folder/Enumerations.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enumerations : MonoBehaviour 
+{
+	enum Direction {Up, Down, Left, Right}
+	
+	void Start () 
+	{
+		Direction myDirection;
+		
+		myDirection = Direction.Left;
+		
+		myDirection = reverse (myDirection);
+	}
+	
+	Direction reverse (Direction dir)
+	{
+		if (dir == Direction.Left)
+		{
+			dir = Direction.Right;
+		}
+		else if (dir == Direction.Right)
+		{
+			dir = Direction.Left;
+		}
+		else if (dir == Direction.Up)
+		{
+			dir = Direction.Down;
+		}
+		else if (dir == Direction.Down)
+		{
+			dir = Direction.Up;
+		}
+		return dir;
+	}
+}
